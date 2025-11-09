b15fb8505f2050d4545d5d307307a8e99dc5c1b1
NIMESHKA MIHIRAN ❤️💪 <nimeshmihiranga183@gmail.com>
2025-11-09 10:56:10 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,5 +1,5 @@
 const PastebinAPI = require('pastebin-js'),
-pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+pastebin = new PastebinAPI('CTxOONQZlqkGpEryUasdKrZbC_aJUiRH')
 const {makeid} = require('./id');
 const QRCode = require('qrcode');
 const express = require('express');
